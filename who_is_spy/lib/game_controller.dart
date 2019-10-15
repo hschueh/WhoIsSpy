502572bd5cbe390c28a19115f8d6e1082f413e8a
hschueh <chueh044@gmail.com>
2019-10-16 04:42:24 +0800

    Playable with hardcoded question. Add basic function to play_page such kill & display players. Should adjust the grid view to display all the player without scroll

--- who_is_spy/lib/game_controller.dart
+++ who_is_spy/lib/game_controller.dart
@@ -11,26 +11,64 @@ class GameController {
   int spyNum; // 1
   bool hasWhite; // 2
 
-  List<String> answer = new List(2);
+  static const List<String> ROLE = ["一般人", "臥底", "白板仔"];
 
+  List<String> answer = new List(3);// [2]固定是哭夭白板仔用的
   List<int> playerList;
+  List<bool> playerStateList; // true: alive, false: dead
+  List<int> rolesLeft = new List(4);
 
   void gameStart() async{
     // Init players role.
     playerList = new List(totalNum);
-    playerList.setAll(0, [0]);
+    playerList.fillRange(0, playerList.length, 0);
     if(hasWhite) {
-      playerList[0] = 2;
-      playerList.setRange(1, spyNum, [1]);
-    } else {
-      playerList.setRange(0, spyNum-1, [1]);
+      playerList[playerList.length-1] = 2;
     }
+    playerList.fillRange(0, spyNum, 1);
     playerList.shuffle();
+    playerStateList = new List(totalNum);
+    playerStateList.fillRange(0, playerStateList.length, true);
+    rolesLeft[0] = totalNum - spyNum - (hasWhite?1:0);
+    rolesLeft[1] = spyNum;
+    rolesLeft[2] = hasWhite?1:0;
+    rolesLeft[3] = totalNum;
     // Players role init finished.
 
-
     // Get a question.
-
+    answer[0] = "中秋節";
+    answer[1] = "端午節";
+    answer[2] = "白板笑你";
     // Somehow get a question finished.
+  }
+
+  void killPlayer(int index) {
+    if(!playerStateList[index])
+      return; // already dead
+    playerStateList[index] = false;
+    rolesLeft[playerList[index]] -= 1;
+    rolesLeft[3] -= 1;
+    if(rolesLeft[3] <= 3) {
+      if(rolesLeft[2] > 0) {
+        // white board wins;
+        print("white board wins");
+      } else if(rolesLeft[1] > 0) {
+        // spy win;
+        print("spy win");
+      } else {
+        // normal guys win;
+        print("normal guys win");
+      }
+    } else if(rolesLeft[1] == 0) {
+      if(rolesLeft[2] > 0) {
+        // white board wins;
+        print("white board wins");
+      } else {
+        // normal guys win;
+        print("normal guys win");
+      }
+    }
+    // do nothing
+    print("Still playing. ${rolesLeft[0]}, ${rolesLeft[1]}, ${rolesLeft[2]}, ${rolesLeft[3]}");
   }
 }
--- who_is_spy/lib/play_page.dart
+++ who_is_spy/lib/play_page.dart
@@ -11,9 +11,62 @@ class PlayPage extends StatefulWidget {
 }
 
 class _PlayPageState extends State<PlayPage> {
+  bool isPeekStage = true;
+  bool isRevealed = false;
+  int peekTurn = 0;
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+    if(isPeekStage) {
+      return Scaffold(
+        body: Center(
+          child:GestureDetector(
+            onDoubleTap: () {
+                setState(() {
+                  isRevealed = !isRevealed;
+                  if(!isRevealed)
+                    peekTurn += 1;
+                    
+                  if(peekTurn == GameController().totalNum)
+                    isPeekStage = false;
+                });
+              },
+            child: Text(isRevealed?GameController().answer[GameController().playerList[peekTurn]]:"請 $peekTurn 號玩家請雙擊"),
+          )
+        )
+      );
+    } else {
+      return Scaffold(
+        body: GridView.count(
+          primary: false,
+          padding: const EdgeInsets.all(20),
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          crossAxisCount: 2,
+          children: generateGrid()
+        )
+      );
+    }
+  }
+
+  List<Widget> generateGrid() {
+    List<Widget> ret = new List<Widget>(GameController().playerList.length);
+    for(int i = 0; i < GameController().playerList.length; ++i) {
+      ret[i] = GestureDetector(
+        onTap: () {
+          if(GameController().playerStateList[i]) {
+            GameController().killPlayer(i);
+            setState((){});
+          } else
+            print("$i Already dead.");
+        },
+        child: Container(
+          color: GameController().playerStateList[i]?Colors.yellow[50]:Colors.grey,
+          child: Center(
+            child: Text('$i號'+(GameController().playerStateList[i]?"":"${GameController.ROLE[GameController().playerList[i]]}")),
+          )
+        )
+      );
+    }
+    return ret;
   }
 }
